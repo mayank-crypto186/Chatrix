60c9d87cb60c52f23dfd0d3d32e9e17ddd982497
mayank-crypto186 <mishra.mayank.010206@gmail.com>
2026-05-10 20:19:27 +0530

    solving deployment issues

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const API = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const signupUser = async (userData) => {
-  return await axios.post(`${API}/signup`, userData);
+  return axios.post(`${API}/signup`, userData);
 };
 
 export const loginUser = async (userData) => {
-  return await axios.post(`${API}/login`, userData);
+  return axios.post(`${API}/login`, userData);
 };
