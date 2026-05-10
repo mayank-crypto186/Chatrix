10969cd29c06f6db66821aff0f9865b317fa8089
mayank-crypto186 <mishra.mayank.010206@gmail.com>
2026-05-10 19:11:47 +0530

    Solving networking issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,15 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.1.44:5173"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
