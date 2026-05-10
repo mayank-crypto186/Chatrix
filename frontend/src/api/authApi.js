02fde007e9963fa0f11836dbe27653c39581df61
mayank-crypto186 <mishra.mayank.010206@gmail.com>
2026-05-10 20:07:44 +0530

    solving deployment issues

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://192.168.1.44:5000/api/auth";
+const API = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const signupUser = async (userData) => {
   return await axios.post(`${API}/signup`, userData);
