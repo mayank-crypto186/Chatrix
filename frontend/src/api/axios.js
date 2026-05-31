007b8e9d88bcf986f6870daf8ade97673aa0d35e
mayank-crypto186 <mishra.mayank.010206@gmail.com>
2026-05-31 17:26:46 +0530

    fix: add auth token interceptor to axios instance

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export default API;
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
